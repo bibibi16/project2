7dc5718303b1defda9c88e119cc7a2b89c9a0efc
bibibi16 <bobo1988kr@gmail.com>
2024-10-18 15:11:33 +0900

    명령어 받는중

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,10 +1,37 @@
 package board;
 
+import java.util.Scanner;
+
+package board;
+
+import java.util.Scanner;
+
 public class Board {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("게시판 프로그램 시작");
-        System.out.println("명령 > ");
-        System.out.println("프로그램을 종료합니다.");
+
+        // 사용자 명령을 처리하기 위한 반복문
+        while (true) {
+            System.out.print("명령 > ");
+            String cmd = scanner.nextLine();
+
+
+            if (cmd.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;  // 반복문 종료
+            } else {
+                // 다른 명령어 처리
+                System.out.println("명령어: " + cmd);
+            }
+        }
+
+        scanner.close();
     }
 }
+
+
+
+
