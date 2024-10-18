d57218286f59a8e2cf04a35002885f489e2afee6
bibibi16 <bobo1988kr@gmail.com>
2024-10-18 15:14:05 +0900

    명령어 받고 있습니다.

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,8 +2,6 @@ package board;
 
 import java.util.Scanner;
 
-package board;
-
 import java.util.Scanner;
 
 public class Board {
@@ -13,13 +11,13 @@ public class Board {
 
         System.out.println("게시판 프로그램 시작");
 
-        // 사용자 명령을 처리하기 위한 반복문
+        // 명령을 처리하기 위한 반복문
         while (true) {
             System.out.print("명령 > ");
             String cmd = scanner.nextLine();
 
 
-            if (cmd.equals("exit")) {
+            if (cmd.equals("exit") || (cmd.equals("종료"))) {
                 System.out.println("프로그램을 종료합니다.");
                 break;  // 반복문 종료
             } else {
